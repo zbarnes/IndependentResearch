2db3b3e2bfba2eef0fbd35ce6466487bba5aed96
Zachary Barnes <33704301+zbarnes@users.noreply.github.com>
2019-05-10 22:44:43 -0700

    Add files via upload

--- exportcev.py
+++ exportcev.py
@@ -7,31 +7,42 @@ import os
 #open file "D:\Exported_From Zack\FileName.Comtrade.Session"
 
 directory = "D:\TwoSevenThree_LEFT_Pending"
+ahk = AHK()
+ac = ActionChain()
 
 for filename in os.listdir(directory):
     if filename.endswith(".cev"):
 	    base = os.path.basename(filename)
 	    split_name = os.path.splitext(base)[0]
+	    #get rid of those annoying commas at the end of some files
 	    if(split_name[len(split_name)-1] == ","):
 		    split_name = split_name[:len(split_name)-1]
 	    print(split_name)
+	    ahk.mouse_move(x=3626, y=25, speed=10, relative = False)
+        ahk.click()
+        ahk.key_down('Down')
+        ac.sleep(1)
+        ahk.key_press('Enter')
+        ac.sleep(1)
+        ahk.type(split_name)
+        ac.sleep(1)
+        ahk.key_press('Enter')
+
+
+
+
+
 	    #print(filename)
 
 
-ahk = AHK()
-ac = ActionChain()
-ahk.mouse_move(x=3626, y=25, speed=10, relative = False)
-ahk.click()
-ahk.key_down('Down')
-ac.sleep(1)
-ahk.key_press('Enter')
-ac.sleep(1)
-ahk.type('Norton smells like poo')
-ac.sleep(1)
-ahk.key_press('Enter')
+
+
 #ac.preform()
 
 
 
 
 #write the name of file as the file name
+
+
+
