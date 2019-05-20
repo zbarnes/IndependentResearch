4dbc953fad07157efc9a9efa4eb286797ed8b2ef
Zachary Barnes <33704301+zbarnes@users.noreply.github.com>
2019-05-20 15:15:16 -0700

    Update exportcev.py
    Working script

--- exportcev.py
+++ exportcev.py
@@ -1,37 +1,52 @@
-from ahk import AHK
 from ahk import ActionChain
 import os
+import subprocess
 
-#os.startfile("C:\Program Files\SEL\SEL SynchroWAVe Event\SynchroWAVeEvent.exe")
-#open file "D:\Exported_From Zack\3 LAKES CKT 12 - 1819 S 111347026301_13_201816_09_58_743_from_cev.Comtrade.Session"
-#open file "D:\Exported_From Zack\FileName.Comtrade.Session"
-
-directory = "D:\TwoSevenThree_LEFT_Pending"
-
-for filename in os.listdir(directory):
-    if filename.endswith(".cev"):
-	    base = os.path.basename(filename)
-	    split_name = os.path.splitext(base)[0]
-	    if(split_name[len(split_name)-1] == ","):
-		    split_name = split_name[:len(split_name)-1]
-	    print(split_name)
-	    #print(filename)
+'''
+When run this script uses autohotkeys with a python wrapper to 
+export .cev files, containing voltage and current fault data from
+distribution systems, to csv format in order to do analysis
+'''
 
 
-ahk = AHK()
 ac = ActionChain()
-ahk.mouse_move(x=3626, y=25, speed=10, relative = False)
-ahk.click()
-ahk.key_down('Down')
-ac.sleep(1)
-ahk.key_press('Enter')
-ac.sleep(1)
-ahk.type('Norton smells like poo')
-ac.sleep(1)
-ahk.key_press('Enter')
-#ac.preform()
 
 
+directory = "D:\\File Path\\"
 
 
-#write the name of file as the file name
+for filename in os.listdir(directory): 
+    if (filename.endswith('CEV') or filename.endswith('cev')):
+        filepath = directory + filename
+	#open .cev file
+        os.startfile(filepath)
+        ac.sleep(3)
+        base = os.path.basename(filename)
+        split_name = os.path.splitext(base)[0]
+        #get rid of file names that end with 
+        if(split_name[len(split_name)-1] == ","):
+           split_name = split_name[:len(split_name)-1]	
+        #end if	
+        #move mouse to export button
+        ac.mouse_move(x=3626, y=25, speed=10, relative = False)
+        ac.sleep(1)
+        ac.click()
+        ac.sleep(1)
+        #navigate to save menu
+        ac.key_down('Down')
+        ac.sleep(1)
+        ac.key_press('Enter')
+        ac.sleep(1)
+        #enter name of file to save
+        ac.type(split_name)
+        ac.sleep(1)
+        ac.key_press('Enter')
+        ac.sleep(1)
+        #close out of current window
+        ac.mouse_move(x=3820, y=20, speed=10, relative = False)
+        ac.sleep(1)
+        ac.click()
+        ac.sleep(1)
+        ac.perform()
+    #end if
+    print(filename)
